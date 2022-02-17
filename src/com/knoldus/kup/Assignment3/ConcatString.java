44b5bcb60a5c27ba0e8f8a4ae971e4bccae582a9
ranurajput <rrajput9760@gmail.com>
2022-02-17 12:27:31 +0530

    Lambda Assignment 3

---
+++ src/com/knoldus/kup/Assignment3/ConcatString.java
@@ -0,0 +1,5 @@
+package com.knoldus.kup.Assignment3;
+@FunctionalInterface
+public interface ConcatString {
+    String changeString(String s1,String s2,String s3,String s4, String s5, String s6, String s7);
+}
---
+++ src/com/knoldus/kup/Assignment3/StringConcatenate.java
@@ -0,0 +1,11 @@
+package com.knoldus.kup.Assignment3;
+import java.util.Locale;
+public class StringConcatenate {
+    public static void main(String... args){
+        // Lambda Expression
+        ConcatString change = (s1,s2,s3,s4,s5,s6,s7) -> (s1+s2+s3+s4+s5+s6+s7).toUpperCase(Locale.ROOT);
+        String s = change.changeString("The", "lambda", "has", "too", "many", "string", "arguments");
+        System.out.println("Compiled by Ranu Rajput");
+        System.out.println("Upper case concatenated String is " + s);
+    }
+}
