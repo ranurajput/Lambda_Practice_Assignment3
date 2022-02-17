b8f46bfc0995bdeff8230a340555ba9c6f8d00e9
RANU RAJPUT <83179328+ranurajput@users.noreply.github.com>
2022-02-17 12:31:01 +0530

    Update StringConcatenate.java

--- src/com/knoldus/kup/Assignment3/StringConcatenate.java
+++ src/com/knoldus/kup/Assignment3/StringConcatenate.java
@@ -6,6 +6,6 @@ public class StringConcatenate {
         ConcatString change = (s1,s2,s3,s4,s5,s6,s7) -> (s1+s2+s3+s4+s5+s6+s7).toUpperCase(Locale.ROOT);
         String s = change.changeString("The", "lambda", "has", "too", "many", "string", "arguments");
         System.out.println("Compiled by Ranu Rajput");
-        System.out.println("Upper case concatenated String is " + s);
+        System.out.println("Upper case concatenated String is " + s); //Output -> Upper case concatenated String is THELAMBDAHASTOOMANYSTRINGARGUMENTS
     }
 }
